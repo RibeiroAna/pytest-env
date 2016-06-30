3eb55bea4f1039a80bdf645281365282a186fb1e
Ana Ribeiro <aninhacostaribeiro@gmail.com>
2016-06-30 16:30:51 +0200

    Changes to pytest_env

--- pytest_env.py
+++ pytest_env.py
@@ -1,47 +1,25 @@
-# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import pytest
 import platform
 
-def pytest_addoption(parser):
-    group = parser.getgroup('env')
-    group.addoption(
-        '--env',
-        action='store_true',
-        help='get the env data'
-    )
-
-
 class Environment:
 
-    def __init__(self, platform, python_version=None, pytest_version=None,
-                 py_version=None, pluggy_version=None, root_dir=None,
-                 ini_file=None, plugins=None):
+    def __init__(self, platform, python_version):
         self.platform = platform
         self.python_version = python_version
-        self.pluggy_version = pluggy_version
-        self.root_dir = root_dir
-        self.ini_file = ini_file
-        self.plugins = plugins
+        self.env_all_var = [platform, python_version]
 
-    def __str__(self):
-        str = "Environment details: "
-        str += "platform={0} ".format(self.platform)
-        str += "python_version={0} ".format(self.python_version)
-        str += "pluggy_version={0} ".format(self.pluggy_version)
-        str += "root_dir={0} ".format(self.root_dir)
-        str += "ini_file={0} ".format(self.ini_file)
-        str += "pluggins={0} ".format(self.plugins)
-        return str
+env_var = Environment (
+                       platform = platform.platform(),
+                       python_version = platform.python_version()
+                      )
 
-environment = Environment (
-                            platform = platform.platform(),
-                            python_version = platform.python_version()
-                          )
-
-@pytest.fixture()
-def get_environment():
-    return environment
-
-def pytest_terminal_summary(terminalreporter):
-        terminalreporter.write_sep('-', environment.__str__())
+@pytest.fixture(scope='session', autouse=True)
+def environment():
+    request.config._environment.extend([
+        ('Python', env_var.python_version()),
+        ('Platform', env_var.platform())])
+    return env_var
